21ac53fad99d5fa094a02daf71cb308e1db1010a
Azat Nizameev <azat.nizam@gmail.com>
2020-04-28 23:24:00 +0300

    Localization

--- src/App.php
+++ src/App.php
@@ -3,6 +3,7 @@ namespace Azatnizam;
 
 use \Azatnizam\AliceRequest;
 use \Azatnizam\MovieSkill;
+use \Symfony\Component\Yaml\Yaml;
 
 class App
 {
@@ -16,15 +17,25 @@ class App
             return;
         }
 
-        $skill->setButton('Help');
+        // localization
+        $mess = Yaml::parseFile(__DIR__ . '/messages.yml');
+
+        $skill->setButton($mess['button.help']);
 
         if ( !is_null( $request->getButtonValue() ) ) {
+
             $skill
-                ->setButton('Clear my history')
-                ->setText('Ok. Type film name or part of the name');
+                ->setButton($mess['button.clear'])
+                ->setText($mess['text.addfilm']);
+
         } elseif ( $request->getCommand() ) {
-            $skill
-                ->setText( 'You command: ' . $request->getCommand() );
+
+            $skill->setText( 'You command: ' . $request->getCommand() );
+
+        } else {
+
+            $skill->setText($mess['text.welcome']);
+
         }
 
         $skill->renderStep();
